15ed271bb1ed5b7e372f8a08c12b001e3eb0c552
Arthur <contato.arthursveloso@gmail.com>
2025-05-20 11:47:53 -0300

    Colocando JavaScript com funções e CSS

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //DECLARANDO FUNÇÃO
+    const loginForm = document.getElementById('form-login');
+    const mensagem = document.getElementById('message');
+    const video = document.querySelector('.video-container');
+    const loginContainer = document.getElementById('login-container');
+    const buttonEntrar = document.getElementById('btnEntrar');
+    const buttonSair= document.getElementById('btnSair');
+    
+})
+
